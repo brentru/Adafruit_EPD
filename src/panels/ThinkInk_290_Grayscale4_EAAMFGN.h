bb0ab6e76e003ce6a2f70b1b75434c92fb0ce183
brentru <brent@adafruit.com>
2026-09-09 13:41:27 -0400

    Bump ThinkInk_290_Grayscale4_EAAMFGN refresh delay

--- src/panels/ThinkInk_290_Grayscale4_EAAMFGN.h
+++ src/panels/ThinkInk_290_Grayscale4_EAAMFGN.h
@@ -126,7 +126,7 @@ class ThinkInk_290_Grayscale4_EAAMFGN : public Adafruit_SSD1680 {
       layer_colors[EPD_LIGHT] = 0b01;
       layer_colors[EPD_DARK] = 0b10;
 
-      default_refresh_delay = 9000;
+      default_refresh_delay = 15000;
       _display_update_val = 0xC7;
 
     } else if (mode == THINKINK_MONO) {
@@ -143,7 +143,7 @@ class ThinkInk_290_Grayscale4_EAAMFGN : public Adafruit_SSD1680 {
       layer_colors[EPD_LIGHT] = 0b00;
       layer_colors[EPD_DARK] = 0b01;
 
-      default_refresh_delay = 800;
+      default_refresh_delay = 15000;
       _display_update_val = 0xC7;
     }
 
